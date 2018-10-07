7251544db439abe9e68f1df2eac996a45002891a
Sowmay <champ.sowmay@gmail.com>
2018-10-06 17:59:20 -0700

    Change Token Expected rate

--- public/js/dapp/instantPay.js
+++ public/js/dapp/instantPay.js
@@ -1,14 +1,3 @@
-// // parameters in this form ETH, DAI, KNC
-// function expectedRateCoinToCoin(coinOneName, coinTwoName) {
-//     var coinOneDecimal = kyberCoinAdd[coinOneName].decimals;
-//     var oneNum = 10 ** coinOneDecimal;
-//     mainKyberContract.getExpectedRate(kyberCoinAdd[coinOneName].contractAddress, kyberCoinAdd[coinTwoName].contractAddress, oneNum, function (err, res) {
-//         if (!err) {
-//             var arr = [String(res[0]), kyberCoinAdd[coinTwoName].decimals];
-//             console.log(arr);
-//             return(arr);
-//         } else {
-//             console.log(err);
-//         };
-//     });
-// }
+function InstaPay() {
+    
+}
--- public/js/dapp/txDapp.js
+++ public/js/dapp/txDapp.js
@@ -1,12 +1,18 @@
 // parameters in this form ETH, DAI, KNC
-function funcToSelect(coinOneName, coinTwoName) {
+function funcToSelect(coinOneName) {
+    console.log(coinOneName);
+    var coinTwoName = "DAI";
     var coinOneDecimal = kyberCoinAdd[coinOneName].decimals;
     var oneNum = 10 ** coinOneDecimal;
-    mainKyberContract.getExpectedRate(kyberCoinAdd[coinOneName].contractAddress, kyberCoinAdd[coinTwoName].contractAddress, oneNum, function (err, res) {
+    var USDAmt = $('#transact_amount').text();
+    console.log(kyberCoinAdd[coinOneName].contractAddress, kyberCoinAdd[coinTwoName].contractAddress, oneNum);
+    mainKyberContract.getExpectedRate(kyberCoinAdd[coinTwoName].contractAddress, kyberCoinAdd[coinOneName].contractAddress, oneNum, function (err, res) {
         if (!err) {
-            var arr = [String(res[0]), kyberCoinAdd[coinTwoName].decimals];
-            console.log(arr);
-            return(arr);
+            var rateVal = String(res[0]);
+            var tknDecimals = kyberCoinAdd[coinTwoName].decimals;
+            var valueToPopulate = rateVal / 10**tknDecimals * USDAmt;
+            console.log(valueToPopulate);
+            $('#transact_value').val(`${valueToPopulate.toFixed(2)} ${coinOneName}`);
         } else {
             console.log(err);
         };
